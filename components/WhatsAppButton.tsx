22755c8c2aeed877484dc825df33e46f354c0c65
surajravidas2312 <surajravidasxxx@gmail.com>
2026-05-02 14:34:02 +0530

    whatsapp number change

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/919000012345"
+      href="https://wa.me/917025152628"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Chat with Codefynix on WhatsApp"
